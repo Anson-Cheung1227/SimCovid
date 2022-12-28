c98d78dd9ec9dabdd1f3c2f8e8e86acbeb9ce543
Anson Cheung <ansoncheung1227@gmail.com>
2022-12-28 18:15:32 +0800

    feat[State]: Removed Airport field
    Use the list's length to check how many airports are in a state.

--- Assets/src/State.cs
+++ Assets/src/State.cs
@@ -6,6 +6,5 @@ public class State : ScriptableObject
 {
     [field: SerializeField] public int Infections {get; private set;}
     [field: SerializeField] public int InHospital {get; private set;}
-    [field: SerializeField] public int Airports {get; private set;}
     [field: SerializeField] public List<Airport> AirportList {get; private set;} = new List<Airport>();
 }
