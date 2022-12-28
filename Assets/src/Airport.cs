fba76c1a30be43aa22beb0c2f5ae747a8addeaad
Anson Cheung <ansoncheung1227@gmail.com>
2022-12-28 18:11:15 +0800

    feat[Airport]: Added Airport ScriptableObjects
    Each airport has its own ScriptableObject.

--- Assets/src/Airport.cs
+++ Assets/src/Airport.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-[System.Serializable]
-public class Airport 
+[CreateAssetMenu(fileName = "New Airport", menuName = "ScriptableObjects/Airport")]
+public class Airport : ScriptableObject
 {
-    [field: SerializeField] public string Name {get; private set;}
-    [field: SerializeField] public string IATACode {get; private set; }  
-    [field: SerializeField] public long DailyPassengers {get; private set;}
-    [field: SerializeField] public string CityServed {get; private set; }      
+    public string Name {get; private set;}
+    public string IATACode {get; private set; }  
+    public long DailyPassengers {get; private set;}
+    public string CityServed {get; private set; }      
 }
