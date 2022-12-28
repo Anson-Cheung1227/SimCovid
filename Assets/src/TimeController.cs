fd7adcc05a97be97a3f111c60e1c4db3bf9cd918
Anson Cheung <ansoncheung1227@gmail.com>
2022-12-28 22:44:49 +0800

    fix[Time]: fixed Generate infection function call bug
    Fixed generation infection from every frame to per day in game time.

--- Assets/src/TimeController.cs
+++ Assets/src/TimeController.cs
@@ -13,7 +13,7 @@ public class TimeController : MonoBehaviour
     public TimeModule.Time GameTime { get; private set; } = new TimeModule.Time(0, 0, 0);
     private void Start()
     {
-        
+
     }
 
     private void Update()
@@ -24,11 +24,11 @@ public class TimeController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha2)) GameSpeed = 2; //1 second (in real life) = 2 hour (in game time)
         if (Input.GetKeyDown(KeyCode.Alpha3)) GameSpeed = 3; //1 second (in real life) = 4 hour (in game time)
         if (Input.GetKeyDown(KeyCode.Alpha4)) GameSpeed = 4; //1 second (in real life) = 8 hour (in game time)
-         _generateInfections.Invoke();
         //Change the Game Speed;
         GameTime += new TimeModule.Time(0, _changeMinutes[GameSpeed] * Time.deltaTime, 0);
         if (GameTime.Hour >= 24)
         {
+            _generateInfections.Invoke();
             //Generate Infections
             GameTime.Hour = 0;
             GameDate += new TimeModule.Date(0, 0, 1);
