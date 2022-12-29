ecdb6a88cb6337a57a4179fa559529710beaddec
Anson Cheung <ansoncheung1227@gmail.com>
2022-12-29 14:33:08 +0800

    fix[Airport]: Corrected mistake in naming properties.

--- Assets/src/Airport.cs
+++ Assets/src/Airport.cs
@@ -5,6 +5,6 @@ public class Airport
 {
     [field: SerializeField] public string Name {get; private set;}
     [field: SerializeField] public string IATACode {get; private set; }  
-    [field: SerializeField] public long DailyPassengers {get; private set;}
+    [field: SerializeField] public long YearlyPassengers {get; private set;}
     [field: SerializeField] public string CityServed {get; private set; }      
 }
