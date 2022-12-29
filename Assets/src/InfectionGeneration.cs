91b71e6919de3aa8894c1a726521cc1011d0a28b
Anson Cheung <ansoncheung1227@gmail.com>
2022-12-29 23:14:25 +0800

    feat[Infection]: Modified InfectionGeneration.cs to be in proportional to the amount of people flowing into the state
    Modified InfectionGeneration.cs to take DailyPassengers of airports into account (InfectionType.Global).
    Modified InfectionGeneration.cs to be compatible with the new Airport class.

--- Assets/src/InfectionGeneration.cs
+++ Assets/src/InfectionGeneration.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class InfectionGeneration : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private List<State> _allState = new List<State>();
+    [SerializeField] private List<StateController> _allState = new List<StateController>();
+    private List<Airport> _allStateAirports = new List<Airport>();
     private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,8 +17,43 @@ public class InfectionGeneration : MonoBehaviour
     {
         
     }
+    //UnityEvent
     public void GenerateInfection()
     {
-        ++_allState[Random.Range(0, _allState.Count - 1)].Infections;
+        AddInfection(DetermineStateInfection(InfectionType.Global), InfectionType.Global);
     }
+    public State DetermineStateInfection(InfectionType infectionType)
+    {
+        if (infectionType == InfectionType.Global)
+        {
+            long DailyTransportedPeople = 0; 
+            Dictionary<long, State> transportedPassengers = new Dictionary<long, State>();
+            State determinedState = null; 
+            foreach (StateController stateController in _allState)
+            {
+                if (stateController.State.DailyIncomingPeople == 0) continue;
+                DailyTransportedPeople += stateController.State.DailyIncomingPeople;
+                transportedPassengers.Add(DailyTransportedPeople, stateController.State);
+            }
+            long person = (long)Random.Range(1L, DailyTransportedPeople);
+            foreach (KeyValuePair<long, State> entry in transportedPassengers)
+            {
+                if (person < entry.Key) 
+                {
+                    determinedState = entry.Value;
+                    break;
+                }
+            }
+            return determinedState;        
+        }
+        return null; 
+    }
+    public void AddInfection(State state, InfectionType infectionType, int infections = 1)
+    {
+        if (infectionType == InfectionType.Global)
+        {
+            state.Infections++; 
+            Debug.Log($"{state.Name}: {infectionType.ToString()}: Generated {infections} infections");
+        }
+    }   
 }
