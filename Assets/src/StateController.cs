0f3d51881609a30f2179751282495f6d6dcde692
Anson Cheung <ansoncheung1227@gmail.com>
2022-12-29 23:11:08 +0800

    feat[State]: Added StateController.cs
    Added StateController.cs to store runtime data of states.
    Also use StateScriptaleObjects as a template on initialization.

---
+++ Assets/src/StateController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class StateController : MonoBehaviour 
+{
+    [field: SerializeField] public State State {get; private set;}
+    [SerializeField] private StateSO _stateTemplate; 
+    void Start()
+    {
+        State = (State)_stateTemplate;
+        foreach (Airport airport in State.AirportList)
+        {
+            State.DailyIncomingPeople += airport.YearlyPassengers/365;
+        }
+    }
+}
