58703315b21f60b6da4987c445d0b6f9ecb9b6ac
Anson Cheung <ansoncheung1227@gmail.com>
2023-01-13 22:22:18 +0800

    refactor: Added comments for InfectionGeneration.cs

--- Assets/src/InfectionGeneration.cs
+++ Assets/src/InfectionGeneration.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class InfectionGeneration : MonoBehaviour
 {
     // Start is called before the first frame update
+    //Reference to TimeController
     [SerializeField] private TimeController _timeController; 
+    //All References to StateControllers for State data
     [SerializeField] private List<StateController> _allState = new List<StateController>();
+    //For Unity Editor use only
     [SerializeField] private List<State> _stateInfections = new List<State>();
+    //Reference to all Airports
     private List<Airport> _allStateAirports = new List<Airport>();
     private void Start()
     {
@@ -34,23 +38,37 @@ public class InfectionGeneration : MonoBehaviour
             _stateInfections.Insert(iter, stateController.State);
         }
     }
-    //UnityEvent
+    //UnityEvent for adding infections, called once per day
     public void GenerateInfection()
     {
+        /*
+            We do not want new generated infections to pass on immediately, else we'll be in an infinite loop
+            We will add all infections at once after the calculations
+        */
         Dictionary<List<State>, long> delayedInfection = new Dictionary<List<State>, long>();
+        //Adding one global infection
         AddInfection(DetermineStateInfectionGlobal(), InfectionType.Global);
+        #region Interstate Infections
+        //Adding Interstate infections
         foreach (StateController stateController in _allState)
         {
+            //If there is no active infections in the state, continue checks for the next state
             if (stateController.State.Infections.Count - stateController.State.InHospital - stateController.State.Recovered == 0) continue;
+            //else, there will be a spread of infections, interstate
             List<State> newInfectState = new List<State>();
+            //Determine the target state
             newInfectState.Add(DetermineStateInfectionInterstate());
+            //Pass the origin state
             newInfectState.Add(stateController.State);
+            //Add to infections queue
             delayedInfection.Add(newInfectState, stateController.State.Infections.Count - stateController.State.InHospital - stateController.State.Recovered);
         }
+        //Loop through the delayedInfection Dictionary, and add infections
         foreach (KeyValuePair<List<State>, long> infection in delayedInfection)
         {
             AddInfection(infection.Key[0], InfectionType.Interstate, infection.Key[1], infection.Value);
         }
+        #endregion Interstate Infections
     }
     public State DetermineStateInfectionInterstate()
     {
@@ -60,16 +78,26 @@ public class InfectionGeneration : MonoBehaviour
     public State DetermineStateInfectionGlobal()
     {
         //TODO: Take in how likely the people are gonna travel 
+        /*
+            This Global infection generation method is temporary,
+            Right now it generates global infection in accordance with the daily transported people, 
+            The higher the daily transported people is, the higher chance it will generate an global infection
+        */
         State determinedState = null;
+        //For calculating the total Transported people per day
         long DailyTransportedPeople = 0;
         Dictionary<long, State> transportedPassengers = new Dictionary<long, State>();
         foreach (StateController stateController in _allState)
         {
+            //If the dailyIncomingPeople is 0, we move on to the next state
             if (stateController.State.DailyIncomingPeople == 0) continue;
+            //else, we add the DailyIncomingPeople to the total DailyTransportedPeople
             DailyTransportedPeople += stateController.State.DailyIncomingPeople;
             transportedPassengers.Add(DailyTransportedPeople, stateController.State);
         }
+        //Determine which person will be infected
         long person = (long)Random.Range(1L, DailyTransportedPeople);
+        //Look up which State the person is arriving to
         foreach (KeyValuePair<long, State> entry in transportedPassengers)
         {
             if (person < entry.Key)
