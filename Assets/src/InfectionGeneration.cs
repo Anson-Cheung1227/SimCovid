c222f92835dff9c64837b6949c4a454c91360654
Anson Cheung <ansoncheung1227@gmail.com>
2023-01-13 19:43:14 +0800

    feat[Infections]: Modified files to use Infection class
    Modified InfectionGeneration.cs and State.cs to use Infection class,
    All infections are stored in a list.

--- Assets/src/InfectionGeneration.cs
+++ Assets/src/InfectionGeneration.cs
@@ -5,55 +5,98 @@ using UnityEngine;
 public class InfectionGeneration : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private TimeController _timeController; 
     [SerializeField] private List<StateController> _allState = new List<StateController>();
+    [SerializeField] private List<State> _stateInfections = new List<State>();
     private List<Airport> _allStateAirports = new List<Airport>();
     private void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        _stateInfections.Clear();
+        foreach (StateController stateController in _allState)
+        {
+            if (_stateInfections.Count == 0) 
+            {
+                _stateInfections.Add(stateController.State);
+                continue;
+            }
+            int iter = _stateInfections.Count;
+            while (stateController.State.Infections.Count > _stateInfections[iter - 1].Infections.Count)
+            {
+                iter--;
+                if (iter == 0) break;
+            }
+            _stateInfections.Insert(iter, stateController.State);
+        }
     }
     //UnityEvent
     public void GenerateInfection()
     {
-        AddInfection(DetermineStateInfection(InfectionType.Global), InfectionType.Global);
-    }
-    public State DetermineStateInfection(InfectionType infectionType)
-    {
-        if (infectionType == InfectionType.Global)
+        Dictionary<List<State>, long> delayedInfection = new Dictionary<List<State>, long>();
+        AddInfection(DetermineStateInfectionGlobal(), InfectionType.Global);
+        foreach (StateController stateController in _allState)
         {
-            long DailyTransportedPeople = 0; 
-            Dictionary<long, State> transportedPassengers = new Dictionary<long, State>();
-            State determinedState = null; 
-            foreach (StateController stateController in _allState)
-            {
-                if (stateController.State.DailyIncomingPeople == 0) continue;
-                DailyTransportedPeople += stateController.State.DailyIncomingPeople;
-                transportedPassengers.Add(DailyTransportedPeople, stateController.State);
-            }
-            long person = (long)Random.Range(1L, DailyTransportedPeople);
-            foreach (KeyValuePair<long, State> entry in transportedPassengers)
-            {
-                if (person < entry.Key) 
-                {
-                    determinedState = entry.Value;
-                    break;
-                }
-            }
-            return determinedState;        
+            if (stateController.State.Infections.Count - stateController.State.InHospital - stateController.State.Recovered == 0) continue;
+            List<State> newInfectState = new List<State>();
+            newInfectState.Add(DetermineStateInfectionInterstate());
+            newInfectState.Add(stateController.State);
+            delayedInfection.Add(newInfectState, stateController.State.Infections.Count - stateController.State.InHospital - stateController.State.Recovered);
         }
-        return null; 
+        foreach (KeyValuePair<List<State>, long> infection in delayedInfection)
+        {
+            AddInfection(infection.Key[0], InfectionType.Interstate, infection.Key[1], infection.Value);
+        }
     }
-    public void AddInfection(State state, InfectionType infectionType, int infections = 1)
+    public State DetermineStateInfectionInterstate()
     {
+        //TODO: Take in how likely the people are gonna travel
+        return _allState[Random.Range(0, _allState.Count - 1)].State;
+    }
+    public State DetermineStateInfectionGlobal()
+    {
+        //TODO: Take in how likely the people are gonna travel 
+        State determinedState = null;
+        long DailyTransportedPeople = 0;
+        Dictionary<long, State> transportedPassengers = new Dictionary<long, State>();
+        foreach (StateController stateController in _allState)
+        {
+            if (stateController.State.DailyIncomingPeople == 0) continue;
+            DailyTransportedPeople += stateController.State.DailyIncomingPeople;
+            transportedPassengers.Add(DailyTransportedPeople, stateController.State);
+        }
+        long person = (long)Random.Range(1L, DailyTransportedPeople);
+        foreach (KeyValuePair<long, State> entry in transportedPassengers)
+        {
+            if (person < entry.Key)
+            {
+                determinedState = entry.Value;
+                break;
+            }
+        }
+        return determinedState;
+    }
+    public void AddInfection(State state, InfectionType infectionType, State originState = null, long infections = 1)
+    {
+        if (infectionType == InfectionType.Interstate)
+        {
+            for (int i = 0; i < infections; ++i)
+            {
+                state.Infections.Add(new Infection{Date = _timeController.GameDate});
+            }
+            Debug.Log($"{state.Name}: {infectionType.ToString()}: Generated {infections} infections from {originState.Name}");
+        }
         if (infectionType == InfectionType.Global)
         {
-            state.Infections++; 
+            for (int i = 0; i < infections; ++i)
+            {
+                state.Infections.Add(new Infection());
+            }
             Debug.Log($"{state.Name}: {infectionType.ToString()}: Generated {infections} infections");
         }
-    }   
+    }
 }
--- Assets/src/State.cs
+++ Assets/src/State.cs
@@ -6,8 +6,10 @@ public class State
 {
     [field: SerializeField] public string Name {get;set;}
     [field: SerializeField] public long Population {get; set;}
+    [field: SerializeField] public float localSpreadRate {get; set;}
     [field: SerializeField] public long DailyIncomingPeople {get; set;}
-    [field: SerializeField] public long Infections {get; set;} 
+    [field: SerializeField] public List<Infection> Infections {get; set;} = new List<Infection>();
     [field: SerializeField] public long InHospital {get; set;}
+    [field: SerializeField] public long Recovered {get; set;}
     [field: SerializeField] public List<Airport> AirportList {get; set;} = new List<Airport>();
 }
