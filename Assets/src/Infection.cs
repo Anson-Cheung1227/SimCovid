650ef45992ea652d90372ea83d17422ab23b05f8
Anson Cheung <ansoncheung1227@gmail.com>
2023-01-13 23:00:58 +0800

    feat[Infection]: Added InfectionStatus enum
    Added InfectionStatus enum to track the status of an Infection,
    used in Infection class.

--- Assets/src/Infection.cs
+++ Assets/src/Infection.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class Infection
 {
     [field: SerializeField] public TimeModule.Date Date {get; set;}
+    [field: SerializeField] public InfectionStatus InfectionStatus {get; set;} 
 }
 public enum InfectionType
 {
     Local,
     Interstate, 
     Global, 
+}
+[System.Serializable]
+public enum InfectionStatus
+{
+    Active,
+    InHospital, 
+    Recovered, 
 }
