174bee9184866c8aff275136c7d1142b2147e298
Anson Cheung <ansoncheung1227@gmail.com>
2023-01-13 19:50:17 +0800

    fix: #4 Lists in StateSO are passed by reference
    Lists in StateSO are now copied to a new List,
    the original value of StateSO will not be modified when modifying values in StateController.
    
    closes: #4

--- Assets/src/StateSO.cs
+++ Assets/src/StateSO.cs
@@ -7,16 +7,21 @@ public class StateSO : ScriptableObject
     [field: SerializeField] public string Name {get; private set;}
     [field: SerializeField] public long Population {get; private set;}
     [field: SerializeField] public long DailyIncomingPeople {get; private set;}
-    [field: SerializeField] public long Infections {get; set;} 
+    [field: SerializeField] public List<Infection> Infections {get; private set;} = new List<Infection>(); 
     [field: SerializeField] public long InHospital {get; private set;}
     [field: SerializeField] public List<AirportSO> AirportList {get; private set;} = new List<AirportSO>(); 
     public static explicit operator State(StateSO stateSO)
     {
+        List<Infection> infections = new List<Infection>();
         List<Airport> airports = new List<Airport>();
+        foreach (Infection infection in stateSO.Infections)
+        {
+            infections.Add(infection);
+        }
         foreach (AirportSO airportSO in stateSO.AirportList)
         {
             airports.Add((Airport)airportSO);
         }
-        return new State{Name = stateSO.Name, Population = stateSO.Population, DailyIncomingPeople = stateSO.DailyIncomingPeople, Infections = stateSO.Infections, InHospital = stateSO.InHospital, AirportList = airports};
+        return new State{Name = stateSO.Name, Population = stateSO.Population, DailyIncomingPeople = stateSO.DailyIncomingPeople, Infections = infections, InHospital = stateSO.InHospital, AirportList = airports};
     }
 }
