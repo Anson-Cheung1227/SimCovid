7514a09b059d456df6a75e51f96627c5ebd6fa81
Anson Cheung <ansoncheung1227@gmail.com>
2023-01-21 13:23:40 +0800

    refactor[State]: Modified InHospital to use List
    State class and StateSO are now using List<Infection> for InHospital to track cases.

--- Assets/src/State.cs
+++ Assets/src/State.cs
@@ -9,7 +9,7 @@ public class State
     [field: SerializeField] public float localSpreadRate {get; set;}
     [field: SerializeField] public long DailyIncomingPeople {get; set;}
     [field: SerializeField] public List<Infection> Infections {get; set;} = new List<Infection>();
-    [field: SerializeField] public long InHospital {get; set;}
+    [field: SerializeField] public List<Infection> InHospital {get; set;} = new List<Infection>();
     [field: SerializeField] public long Recovered {get; set;}
     [field: SerializeField] public List<Airport> AirportList {get; set;} = new List<Airport>();
 }
--- Assets/src/StateSO.cs
+++ Assets/src/StateSO.cs
@@ -8,20 +8,26 @@ public class StateSO : ScriptableObject
     [field: SerializeField] public long Population {get; private set;}
     [field: SerializeField] public long DailyIncomingPeople {get; private set;}
     [field: SerializeField] public List<Infection> Infections {get; private set;} = new List<Infection>(); 
-    [field: SerializeField] public long InHospital {get; private set;}
+    [field: SerializeField] public List<Infection> InHospital {get; private set;} = new List<Infection>();
     [field: SerializeField] public List<AirportSO> AirportList {get; private set;} = new List<AirportSO>(); 
     public static explicit operator State(StateSO stateSO)
     {
         List<Infection> infections = new List<Infection>();
+        List<Infection> inHospital = new List<Infection>();
         List<Airport> airports = new List<Airport>();
+        //Copy the ScriptableObject into a new List to avoid changing the lists in ScriptableObjects.
         foreach (Infection infection in stateSO.Infections)
         {
             infections.Add(infection);
+        }
+        foreach (Infection infection in stateSO.InHospital)
+        {
+            inHospital.Add(infection);
         }
         foreach (AirportSO airportSO in stateSO.AirportList)
         {
             airports.Add((Airport)airportSO);
         }
-        return new State{Name = stateSO.Name, Population = stateSO.Population, DailyIncomingPeople = stateSO.DailyIncomingPeople, Infections = infections, InHospital = stateSO.InHospital, AirportList = airports};
+        return new State{Name = stateSO.Name, Population = stateSO.Population, DailyIncomingPeople = stateSO.DailyIncomingPeople, Infections = infections, InHospital = inHospital, AirportList = airports};
     }
 }
