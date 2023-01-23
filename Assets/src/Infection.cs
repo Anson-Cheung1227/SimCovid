9e997cfa3d3889e217d30ee9152ba35163912a30
Anson Cheung <ansoncheung1227@gmail.com>
2023-01-23 11:09:58 +0800

    feat[Infection]: Modified Infection class for a new system
    Infection class now holds a set of infections with same values, represented by Amount variable.
    Added FindExistingInfection() to find if there is an existing Infection that holds the same values.

--- Assets/src/Infection.cs
+++ Assets/src/Infection.cs
@@ -1,31 +1,75 @@
 using UnityEngine;
-[System.Serializable]
-public class Infection
+
+namespace InfectionModule
 {
-    [field: SerializeField] public TimeModule.Date Date {get; set;}
-    [field: SerializeField] public InfectionStatus InfectionStatus {get; set;} 
-    [field: SerializeField] public int hasSpread {get; set;}
-}
-[System.Serializable]
-public struct InfectionStruct
-{
-    [field: SerializeField] public TimeModule.Date Date {get; set;}
-    [field: SerializeField] public InfectionStatus InfectionStatus {get; set;} 
-    public static explicit operator Infection(InfectionStruct infectionStruct)
+
+    [System.Serializable]
+    public class Infection
     {
-        return new Infection{Date = infectionStruct.Date, InfectionStatus = infectionStruct.InfectionStatus};
+        [field: SerializeField] public TimeModule.Date Date { get; set; }
+        [field: SerializeField] public InfectionStatus InfectionStatus { get; set; }
+        [field: SerializeField] public long Amount { get; set; }
+        public static Infection FindExistingInfection(State state, TimeModule.Date date, InfectionStatus infectionStatus)
+        {
+            Infection findResult = null;
+            if (infectionStatus == InfectionStatus.Active)
+            {
+                foreach (InfectionModule.Infection infection in state.ActiveInfections)
+                {
+                    if (date == infection.Date)
+                    {
+                        findResult = infection;
+                        break;
+                    }
+                }
+            }
+            else if (infectionStatus == InfectionStatus.InHospital)
+            {
+                foreach (Infection infection in state.InHospital)
+                {
+                    if (date == infection.Date)
+                    {
+                        findResult = infection;
+                        break;
+                    }
+                }
+            }
+            else if (infectionStatus == InfectionStatus.Recovered)
+            {
+                foreach (Infection infection in state.InHospital)
+                {
+                    if (date == infection.Date)
+                    {
+                        findResult = infection;
+                        break;
+                    }
+                }
+            }
+            return findResult;
+        }
     }
-}
-public enum InfectionType
-{
-    Local,
-    Interstate, 
-    Global, 
-}
-[System.Serializable]
-public enum InfectionStatus
-{
-    Active,
-    InHospital, 
-    Recovered, 
+    [System.Serializable]
+    public struct InfectionStruct
+    {
+        [field: SerializeField] public TimeModule.Date Date { get; set; }
+        [field: SerializeField] public InfectionStatus InfectionStatus { get; set; }
+        [field: SerializeField] public long Amount { get; set; }
+        public static explicit operator Infection(InfectionStruct infectionStruct)
+        {
+            return new Infection { Date = infectionStruct.Date, InfectionStatus = infectionStruct.InfectionStatus, Amount = infectionStruct.Amount };
+        }
+    }
+    public enum InfectionType
+    {
+        Local,
+        Interstate,
+        Global,
+    }
+    [System.Serializable]
+    public enum InfectionStatus
+    {
+        Active,
+        InHospital,
+        Recovered,
+    }
 }
