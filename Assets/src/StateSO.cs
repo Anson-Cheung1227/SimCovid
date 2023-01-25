f46f402e2d230cf8ab37ebe7ebbbe047ef0321bb
Anson Cheung <ansoncheung1227@gmail.com>
2023-01-25 23:47:35 +0800

    feat[StateScriptableObjects]: StateSO will now copy (long) values
    StateSO will now copy (long) values during conversion to State class.

--- Assets/src/StateSO.cs
+++ Assets/src/StateSO.cs
@@ -7,7 +7,7 @@ public class StateSO : ScriptableObject
 {
     [field: SerializeField] public string Name {get; private set;}
     [field: SerializeField] public long Population {get; private set;}
-    [field: SerializeField] public float localSpreadRate {get; set;}
+    [field: SerializeField] public float LocalSpreadRate {get; set;}
     [field: SerializeField] public long DailyIncomingPeople {get; private set;}
     [field: SerializeField] public List<InfectionStruct> Infections {get; private set;} = new List<InfectionStruct>(); 
     [field: SerializeField] public List<AirportSO> AirportList {get; private set;} = new List<AirportSO>(); 
@@ -17,18 +17,25 @@ public class StateSO : ScriptableObject
         List<Infection> activeInfections = new List<Infection>();
         List<Infection> inHospital = new List<Infection>();
         List<Airport> airports = new List<Airport>();
+        long infectionsLong = 0;
+        long activeInfectionsLong = 0;
+        long inHospitalLong = 0;
+        long recoveredLong = 0;
         //Copy the ScriptableObject into a new List to avoid changing the lists in ScriptableObjects.
         foreach (InfectionStruct infection in stateSO.Infections)
         {
             Infection refInfection = (Infection)infection;
             infections.Add(refInfection); 
+            infectionsLong += infection.Amount;
             switch (infection.InfectionStatus)
             {
                 case InfectionStatus.Active:
                     activeInfections.Add(refInfection);
+                    activeInfectionsLong += infection.Amount;
                     break;
                 case InfectionStatus.InHospital:
                     inHospital.Add(refInfection);
+                    inHospitalLong += infection.Amount;
                     break;
             }
         }
@@ -36,6 +43,19 @@ public class StateSO : ScriptableObject
         {
             airports.Add((Airport)airportSO);
         }
-        return new State{Name = stateSO.Name, Population = stateSO.Population, localSpreadRate = stateSO.localSpreadRate ,DailyIncomingPeople = stateSO.DailyIncomingPeople, Infections = infections,ActiveInfections = activeInfections ,InHospital = inHospital, AirportList = airports};
+        return new State
+        {
+            Name = stateSO.Name, 
+            Population = stateSO.Population, 
+            LocalSpreadRate = stateSO.LocalSpreadRate ,
+            DailyIncomingPeople = stateSO.DailyIncomingPeople, 
+            Infections = infections,
+            ActiveInfections = activeInfections,
+            InHospital = inHospital, 
+            InfectionsLong = infectionsLong,
+            ActiveInfectionsLong = activeInfectionsLong,
+            InHospitalLong = inHospitalLong,
+            AirportList = airports
+        };
     }
 }
