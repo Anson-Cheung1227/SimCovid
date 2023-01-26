ba84fe7495d8a29801444027df39cbb414f1cfc5
Anson Cheung <ansoncheung1227@gmail.com>
2023-01-26 17:34:15 +0800

    feat[Camera]: Camera will not move if mouse is on UI
    Using Raycast, if the user drag on UI elements, the Camera will not move.

--- Assets/src/CameraController.cs
+++ Assets/src/CameraController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour 
 {
@@ -8,22 +10,39 @@ public class CameraController : MonoBehaviour
     private Vector3 _difference;
     private Vector3 _resultPosition;
     private bool _isDrag = false;
+    [SerializeField] private LayerMask _uiLayerMask;
     private void LateUpdate() 
     {
         if (Input.GetMouseButton(0))
         {
-            _difference = _mainCamera.ScreenToWorldPoint(Input.mousePosition) - _mainCamera.transform.position;
-            //check if this is the first click
-            if (!_isDrag)
+            if (!isPointerOverUI())
             {
-                //record the original position if it is the firt click
-                _isDrag = true;
-                _origin = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                _difference = _mainCamera.ScreenToWorldPoint(Input.mousePosition) - _mainCamera.transform.position;
+                if (!_isDrag)
+                {
+                    _isDrag = true;
+                    _origin = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                }
             }
             _resultPosition =  _origin - _difference;
             _resultPosition.z = -10;
             _mainCamera.transform.position = _resultPosition;
         }
         else _isDrag = false;
+    }
+    private bool isPointerOverUI()
+    {
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        pointerEventData.position = Input.mousePosition;
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+        for (int i = 0; i < raycastResults.Count; ++i)
+        {
+            if ((raycastResults[i].gameObject.layer & (1 << _uiLayerMask)) != 0)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
