193f3a098ffc6894e852a876e756434c036a325d
Anson Cheung <ansoncheung1227@gmail.com>
2023-01-27 22:01:24 +0800

    feat[Lockdown]: Added buttons for changing lockdown
    LockdownManager.cs will change the lockdown status when the button is pressed

---
+++ Assets/src/LockdownManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockdownManager : MonoBehaviour
+{
+    [SerializeField] DataManager _dataManager;
+    [SerializeField] GameObject _localLockdownButton;
+    [SerializeField] GameObject _interstateLockdownButton;
+    [SerializeField] GameObject _globalLockdownButton;
+    public void OnLockdownUpdateButtonClick(GameObject button)
+    {
+        if (button == _localLockdownButton) _dataManager.SelectedState.LocalLockdown = !_dataManager.SelectedState.LocalLockdown;
+        if (button == _interstateLockdownButton) _dataManager.SelectedState.InterstateLockdown = !_dataManager.SelectedState.InterstateLockdown;
+        if (button == _globalLockdownButton) _dataManager.SelectedState.GlobalLockdown = !_dataManager.SelectedState.GlobalLockdown;
+    }
+}
