2667c359b2f754d738c8d7f1cc2680b040516cbb
Anson Cheung <ansoncheung1227@gmail.com>
2023-02-17 14:04:14 +0800

    refactor: Added naming conventions.
    Renamed files according to naming conventions

---
+++ Assets/FILE_NAMING.md
@@ -0,0 +1,17 @@
+# File Naming
+
+File naming conventions in this project is as follows:
+
+### **SO**: Scriptable Objects
+
+### **MB**: MonoBehavior
+
+### **PRE**: Prefabs
+
+### **T**: Texture
+
+### **AU**: Audio
+
+### **PS**: Particle System
+
+**All naming conventions are subjected to change. 
---
+++ Assets/src/MB_MoraleManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoraleManager : MonoBehaviour
+{
+    [SerializeField] private List<StateController> _allStates; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateMorale()
+    {
+        foreach (StateController stateController in _allStates)
+        {
+            stateController.State.Morale = ((float)1 - (float)stateController.State.InfectionsLong / (float)stateController.State.Population) * (float)100;
+        }
+    }
+}
