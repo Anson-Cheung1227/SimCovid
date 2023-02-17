b596fd6faa1b89cc8426ccac591fc9dab9435095
Anson Cheung <ansoncheung1227@gmail.com>
2023-02-17 15:47:56 +0800

    feat[UI]: Added loading screen

--- Assets/src/DataManager.cs
+++ Assets/src/DataManager.cs
@@ -4,10 +4,10 @@ using System;
 public class DataManager : MonoBehaviour 
 {
     public static DataManager Instance; 
-    public DateTime GameDateTime;
+    public DateTime GameDateTime = new DateTime(2019, 12, 1, 0, 0, 0);
     public bool ActiveStateDetailsPanel;
     public State HoveringState;
-    public State SelectedState;
+    public State SelectedState = null;
     public float RecoveryRate;
     public float DeathRate;
     [SerializeField] private GlobalStatsSO _globalStatsSO;  
@@ -17,8 +17,6 @@ public class DataManager : MonoBehaviour
     }
     void Start()
     {
-        GameDateTime = new DateTime(2019,12,1,1,1,1);
-        SelectedState = null;
         RecoveryRate = _globalStatsSO.RecoveryRate;
         DeathRate = _globalStatsSO.DeathRate;
     }  
--- Assets/src/GameManager.cs
+++ Assets/src/GameManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private CoreGameSO _coreGame;
+    [SerializeField] private GameObject _loadingScreen;
+    [SerializeField] private Image _progressBar;
+    private List<AsyncOperation> _scenesLoading = new List<AsyncOperation>();
+    private float totalProgress = 0;
     public static GameManager Instance; 
     // Start is called before the first frame update
     private void Awake() 
@@ -25,9 +29,33 @@ public class GameManager : MonoBehaviour
     }
     public void LoadLevel(CoreGameSO coreGameSO)
     {
+        _loadingScreen.SetActive(true);
+        _scenesLoading.Clear();
         foreach (SceneEnum sceneEnum in coreGameSO.SceneList)
         {
-            SceneManager.LoadSceneAsync((int)sceneEnum, LoadSceneMode.Additive);
+            _scenesLoading.Add(SceneManager.LoadSceneAsync((int)sceneEnum, LoadSceneMode.Additive));
         }
+        StartCoroutine(GetSceneLoadingProgress());
+    }
+    public IEnumerator GetSceneLoadingProgress()
+    {
+        for (int i = 0; i < _scenesLoading.Count; ++i)
+        {
+            totalProgress = 0;
+            foreach (AsyncOperation asyncOperation in _scenesLoading)
+            {
+                totalProgress += asyncOperation.progress;
+                Debug.Log(asyncOperation.progress);
+            }
+            totalProgress = (totalProgress / _scenesLoading.Count);
+            _progressBar.fillAmount = totalProgress;
+            while (!_scenesLoading[i].isDone)
+            {
+                yield return null;
+            }
+        }
+        totalProgress = 1;
+        _progressBar.fillAmount = totalProgress;
+        _loadingScreen.SetActive(false);
     }
 }
