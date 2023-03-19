0e8caf9f1b01c551fe1681f7d13ee262d5770dda
Anson Cheung <ansoncheung1227@gmail.com>
2023-03-19 17:57:48 +0800

    refactor: Added Assembly definitions and core mod
    Added ISimCovid Assembly definitions

---
+++ SimCovidCoreMod/CoreMod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISimCovid;
+
+namespace SimCovidCore
+{
+    public class CoreMod : IMod
+    {
+        public string Name { get { return "Name"; } }
+        public string Description { get { return "Description"; } }
+    }
+}
