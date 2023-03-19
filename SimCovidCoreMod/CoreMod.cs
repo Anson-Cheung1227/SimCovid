4ec9c36408070eee6f57bb4119a9719278c3c8de
Anson Cheung <ansoncheung1227@gmail.com>
2023-03-19 18:26:06 +0800

    feat[Mod]: Added ISimCovid interfaces

--- SimCovidCoreMod/CoreMod.cs
+++ SimCovidCoreMod/CoreMod.cs
@@ -7,7 +7,7 @@ using ISimCovid;
 
 namespace SimCovidCore
 {
-    public class CoreMod : IMod
+    public class CoreMod : IMod, ISpreadable
     {
         public string Name { get { return "Name"; } }
         public string Description { get { return "Description"; } }
