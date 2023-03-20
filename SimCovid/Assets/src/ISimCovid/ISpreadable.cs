58d0cc00f885b6facd0f98d66b4270fde0124446
Anson Cheung <ansoncheung1227@gmail.com>
2023-03-20 17:30:11 +0800

    feat[Infection]: Added ISpreadable and ISpreadableStatus

--- SimCovid/Assets/src/ISimCovid/ISpreadable.cs
+++ SimCovid/Assets/src/ISimCovid/ISpreadable.cs
@@ -8,5 +8,16 @@ namespace ISimCovid
 {
     public interface ISpreadable
     {
+        public DateTime Date { get; }
+        public Nullable<DateTime> InHospitalDate { get; }
+        public Nullable<DateTime> RecoveryDate { get; }
+        public Nullable<DateTime> DeceasedDate { get; }
+        public long Amount{ get; }
+        public bool HasSpread { get; }
+        public void AddToInfection(long amount);
+        public void SetInHospitalDate(DateTime date);
+        public void SetRecoveryDate(DateTime date);
+        public void SetDeceasedDate(DateTime date);
+        public ISpreadable FindExistingInfection(object list, object spreadableClass);
     }
 }
---
+++ SimCovid/Assets/src/ISimCovid/ISpreadableStatus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISimCovid
+{
+    public interface ISpreadableStatus
+    {
+        public string StatusName { get; }
+        public int StatusValue { get; }
+    }
+    public class InfectionStatus
+    {
+        public static readonly ActiveInfection ActiveInfectionStatus = new ActiveInfection();
+        public static readonly InHospitalInfection InHospitalInfectionStatus = new InHospitalInfection();
+        public static readonly RecoveredInfection RecoveredInfectionStatus = new RecoveredInfection();
+        public static readonly DeceasedInfection DeceasedInfectionStatus = new DeceasedInfection();
+        public class ActiveInfection : ISpreadableStatus
+        {
+            public string StatusName { get { return "Active"; } }
+            public int StatusValue { get { return 0; } }
+        }
+        public class InHospitalInfection : ISpreadableStatus
+        {
+            public string StatusName { get { return "InHospital"; } }
+
+            public int StatusValue { get { return 1; } }
+        }
+        public class RecoveredInfection : ISpreadableStatus
+        {
+            public string StatusName { get { return "Recovereed"; } }
+            public int StatusValue { get { return 2; } }
+        }
+        public class DeceasedInfection : ISpreadableStatus
+        { 
+            public string StatusName { get { return "Deceased"; } } 
+            public int StatusValue { get { return 3; } }
+        }
+    }
+}
