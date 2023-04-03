7882de99276c85c07cfa3c008b3c1801108cb8b8
mr.devpotato <67113818+over1548@users.noreply.github.com>
2023-04-03 10:40:45 +0000

    Zoom

---
+++ SimCovid/Assets/Reivew/Zoom.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    public Slider slider;
+    public Transform map;
+
+    public float multiplier;
+    string mapTag = "Map";
+
+    void Start()
+    {
+        map = GameObject.FindGameObjectWithTag(mapTag).GetComponent<Transform>();
+    }
+
+    public void FixedUpdate()
+    {
+
+        float mapSize = slider.value * multiplier;
+        Debug.Log(mapSize);
+        map.localScale = new Vector2(mapSize, mapSize);
+         
+    }
+    
+}
