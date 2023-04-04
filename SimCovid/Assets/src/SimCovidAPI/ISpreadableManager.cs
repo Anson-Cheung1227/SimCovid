9afb2e23c68c6af23fb77cb179ca838b7d1df99f
Anson Cheung <ansoncheung1227@gmail.com>
2023-04-04 10:11:27 +0800

    refactor: Removed generics for ISpreadableManager

--- SimCovid/Assets/src/SimCovidAPI/ISpreadableManager.cs
+++ SimCovid/Assets/src/SimCovidAPI/ISpreadableManager.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace SimCovidAPI
 {
-    public interface ISpreadableManager<ISpreadableTarget> where ISpreadableTarget : class, ISpreadable, new()
+    public interface ISpreadableManager
     {
         public long Limit { get; }
-        public IEnumerable<ISpreadableDataHandler<ISpreadableTarget>> GetAll();
-        public ISpreadableDataHandler<ISpreadableTarget> GetActive();
-        public ISpreadableDataHandler<ISpreadableTarget> GetInHospital();
-        public ISpreadableDataHandler<ISpreadableTarget> GetRecovered();
-        public ISpreadableDataHandler<ISpreadableTarget> GetDeceased();
+        public IEnumerable<ISpreadableDataHandler> GetAll();
+        public ISpreadableDataHandler GetActive();
+        public ISpreadableDataHandler GetInHospital();
+        public ISpreadableDataHandler GetRecovered();
+        public ISpreadableDataHandler GetDeceased();
         public long GetTotalInfections();
     }
 }
