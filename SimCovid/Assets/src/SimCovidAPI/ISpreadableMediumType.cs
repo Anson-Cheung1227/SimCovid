76f55c69376e6ad27763dd31ace52ca766caaad6
Anson Cheung <ansoncheung1227@gmail.com>
2023-06-23 23:02:52 +0800

    refactor: Added comments for InfectionMediumType.cs and ISpreadableMediumType.cs

--- SimCovid/Assets/src/SimCovidAPI/ISpreadableMediumType.cs
+++ SimCovid/Assets/src/SimCovidAPI/ISpreadableMediumType.cs
@@ -1,5 +1,8 @@
 ﻿namespace SimCovidAPI
 {
+    /// <summary>
+    /// Represents the Medium of infection, how the infection occured, through what means (for example, Local, interstate, global)
+    /// </summary>
     public interface ISpreadableMediumType
     {
         public string MediumName { get; }
--- SimCovid/Assets/src/SimCovidAPI/InfectionMediumType.cs
+++ SimCovid/Assets/src/SimCovidAPI/InfectionMediumType.cs
@@ -1,11 +1,17 @@
 ﻿namespace SimCovidAPI
 {
+    /// <summary>
+    /// Class for built-in ISpreadableMediumTypes.
+    /// </summary>
     public class InfectionMediumType
     {
         //Zero and Negative Numbers are reserved for Error uses.
         public static readonly LocalMediumType Local = new LocalMediumType();
         public static readonly InterstateMediumType Interstate = new InterstateMediumType();
         public static readonly GlobalMediumType Global = new GlobalMediumType();
+        /// <summary>
+        /// Local infection. The infection occured locally.
+        /// </summary>
         public class LocalMediumType : ISpreadableMediumType
         {
             public string MediumName { get { return "Local"; } }
@@ -13,7 +19,9 @@
             public static implicit operator string(LocalMediumType localMediumType) => localMediumType.MediumName;
             public static implicit operator int(LocalMediumType localMediumType) => localMediumType.MediumValue;
         }
-
+        /// <summary>
+        /// Interstate infection. The infection occured through traveling across states.
+        /// </summary>
         public class InterstateMediumType : ISpreadableMediumType
         {
             public string MediumName { get { return "Interstate"; } }
@@ -21,7 +29,9 @@
             public static implicit operator string(InterstateMediumType interstateMediumType) => interstateMediumType.MediumName;
             public static implicit operator int(InterstateMediumType interstateMediumType) => interstateMediumType.MediumValue;
         }
-
+        /// <summary>
+        /// Global infection. The infection occured through traveling across countries. 
+        /// </summary>
         public class GlobalMediumType : ISpreadableMediumType
         {
             public string MediumName { get { return "Global"; } }
