8d12199516f596b212df6f2326ae33a5c235cc34
Anson Cheung <ansoncheung1227@gmail.com>
2023-06-25 00:29:45 +0800

    feat[Policy]: Added PolicyDefaultTypes

---
+++ SimCovid/Assets/src/SimCovidAPI/Locations/Policies/PolicyDefaultTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace SimCovidAPI.Locations.Policies
+{
+    public class PolicyDefaultTypes
+    {
+        public static LockdownPolicyType Lockdown = new LockdownPolicyType();
+        public class LockdownPolicyType : IPolicyType
+        {
+            public string PolicyName { get { return "Lockdown"; } }
+            public string PolicyDescription { get { return "Citizens are not allowed to move locally, unless emergencies"; } }
+
+            public static implicit operator string(LockdownPolicyType lockdownPolicyType) =>
+                lockdownPolicyType.PolicyName;
+        }
+    }
+}
