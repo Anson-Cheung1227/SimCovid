6448218f3c791c71e0153e0ae258a16827026635
Anson Cheung <ansoncheung1227@gmail.com>
2023-06-25 00:27:13 +0800

    feat[Policy]: Added Policy interfaces

---
+++ SimCovid/Assets/src/SimCovidAPI/Locations/Policies/IPolicy.cs
@@ -0,0 +1,9 @@
+﻿namespace SimCovidAPI.Locations.Policies
+{
+    public interface IPolicy
+    {
+        public IPolicyType PolicyType { get; }
+        public bool Active { get; }
+        public void SetActive(bool active);
+    }
+}
---
+++ SimCovid/Assets/src/SimCovidAPI/Locations/Policies/IPolicyManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace SimCovidAPI.Locations.Policies
+{
+    public interface IPolicyManager
+    {
+        public IEnumerable GetAll();
+        public IPolicy GetPolicy(string name);
+    }
+}
