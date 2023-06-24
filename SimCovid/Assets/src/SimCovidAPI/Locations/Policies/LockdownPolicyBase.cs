5c323aa323618b722e85bc564acd0149a9164121
Anson Cheung <ansoncheung1227@gmail.com>
2023-06-25 00:31:10 +0800

    feat[Policy]: Added policy base classes

---
+++ SimCovid/Assets/src/SimCovidAPI/Locations/Policies/LockdownPolicyBase.cs
@@ -0,0 +1,9 @@
+﻿namespace SimCovidAPI.Locations.Policies
+{
+    public abstract class LockdownPolicyBase : PolicyBase
+    {
+        public LockdownPolicyBase(bool active = false) : base(new PolicyDefaultTypes.LockdownPolicyType(), active)
+        {
+        }
+    }
+}
---
+++ SimCovid/Assets/src/SimCovidAPI/Locations/Policies/PolicyBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SimCovidAPI.Locations.Policies
+{
+    public abstract class PolicyBase : IPolicy
+    {
+        protected readonly IPolicyType p_PolicyType;
+        public virtual IPolicyType PolicyType { get { return p_PolicyType; }}
+        public virtual bool Active { get; protected set; }
+        public void SetActive(bool active)
+        {
+            Active = active;
+        }
+
+        public PolicyBase(IPolicyType policyType, bool active = false)
+        {
+            p_PolicyType = policyType;
+            Active = active;
+        }
+    }
+}
