30296dcd2d4ed0a0ed51e0f4cf473f19268fd285
Anson Cheung <ansoncheung1227@gmail.com>
2023-06-25 22:11:59 +0800

    Reset commits

---
+++ SimCovid/Assets/src/Core/Policies/EligibilityManager.cs
@@ -0,0 +1,11 @@
+﻿using SimCovidAPI.Locations;
+
+namespace SimCovid.Core.Policies
+{
+    public class EligibilityManager : EligibilityManagerBase
+    {
+        public EligibilityManager(ILocation location) : base(new LocalEligibilityCalculator(location))
+        {
+        }
+    }
+}
