dfe50f993a9f8f0aa003b0679153b3984f673022
Anson Cheung <ansoncheung1227@gmail.com>
2023-06-25 10:06:57 +0800

    feat[Policy]: Added Eligibility interfaces, classes

---
+++ SimCovid/Assets/src/SimCovidAPI/Locations/EligibilityManagerBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SimCovidAPI.Locations
+{
+    public abstract class EligibilityManagerBase : IEligibilityManager
+    {
+        protected readonly Dictionary<int,IEligibilityCalculator> EligibilityDictionary;
+
+        public EligibilityManagerBase(IEligibilityCalculator local)
+        {
+            EligibilityDictionary = new Dictionary<int, IEligibilityCalculator>();
+            EligibilityDictionary.Add(local.MediumType.MediumValue, local);
+        }
+        public virtual bool GetEligibility(int key)
+        {
+            return EligibilityDictionary[key].Calculate();
+        }
+    }
+}
---
+++ SimCovid/Assets/src/SimCovidAPI/Locations/IEligibilityCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace SimCovidAPI.Locations
+{
+    /// <summary>
+    /// Calculates the eligibility for spread of infection at a location
+    /// </summary>
+    public interface IEligibilityCalculator
+    {
+        public ISpreadableMediumType MediumType { get; }
+        /// <summary>
+        /// Returns the eligibility of a location
+        /// </summary>
+        /// <returns>Eligibility of a location</returns>
+        public bool Calculate();
+    }
+}
